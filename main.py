c1f1085d64e7ba3dc77015479274a94c1162d5af
salyeoneun-rgb <sal.yeoneun@gmail.com>
2026-08-07 10:56:33 +0900

    feat: initialize FastAPI project

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(
+    title="VN Study Journal",
+    version="0.1.0"
+)
+
+# Static Files
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
+# Templates
+templates = Jinja2Templates(directory="app/templates")
+
+
+from app.routers import home
+
+app.include_router(home.router)
